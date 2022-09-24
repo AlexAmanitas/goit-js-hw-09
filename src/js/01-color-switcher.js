1242b0781a146318fece1453dc4b204d386fcec1
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-09-24 14:43:38 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let changeId = null;
+
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  setBodyColor();
+  changeId = setInterval(setBodyColor, 1000);
+}
+
+function stopChangeColor() {
+  clearInterval(changeId);
+}
+
+function setBodyColor() {
+  refs.body.style.background = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
